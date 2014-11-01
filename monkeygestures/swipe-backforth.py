af728a3df84450d41c2f85df24b3704b822c1c30
stanley zheng <dev.stanzheng@gmail.com>
2014-11-01 16:47:43 -0400

    swipe-leftirhgt

--- monkeygestures/swipe-backforth.py
+++ monkeygestures/swipe-backforth.py
@@ -10,15 +10,13 @@ device = MonkeyRunner.waitForConnection()
 device.touch(100, 500, MonkeyDevice.DOWN)
 
 # A swipe left from (x1, y) to (x2, y) in 2 steps
-center = 400 #center
+y = 400 #center
 x1 = 100#left
 x2 = 400#right
-y2 = 600#up
-y3 = 200#down
 
-startfromleft = (x1, center)
-endfromright = (x2, center)
 
+start = (x1, y)
+end = (x2, y)
 duration = 0.2
 steps = 2
 pause = 0.2
@@ -26,16 +24,12 @@ pause = 0.2
 for i in range(1, 250):
     # Every so often inject a touch to spice things up!
     if i % 9 == 0:
-        device.touch(x2, center, 'UP_AND_DOWN')
+        device.touch(x2, y, 'DOWN_AND_UP')
         MonkeyRunner.sleep(pause)
     # Swipe right
     device.drag(start, end, duration, steps)
     MonkeyRunner.sleep(pause)
     # Swipe left
-    device.drag(end, start, duration, steps)
-    MonkeyRunner.sleep(pause)
-
-    # Swipe up
     device.drag(end, start, duration, steps)
     MonkeyRunner.sleep(pause)
 
